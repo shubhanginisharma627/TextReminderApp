e3036da482ea86e65ec9af58b055734f058cb32d
ShubhanginiSharma627 <sharma546shubu@gmail.com>
2023-11-17 15:23:28 +0530

    Initial Commit

---
+++ TextReminderApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TextReminderApp
+//
+//  Created by Shubhangini Sharma on 17/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TextReminderApp/TextReminderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextReminderAppApp.swift
+//  TextReminderApp
+//
+//  Created by Shubhangini Sharma on 17/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TextReminderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
